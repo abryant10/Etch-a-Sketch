982061e3aa4eab107c406648427a7492e3a57024
Alex Bryant <alexanderpbryant@gmail.com>
2021-03-04 17:04:30 -0600

    grid change size promnpt working

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.getElementById('container');
-let gridNumber = 20;
+const clearButton = document.querySelector('.clearButton');
+let gridNumber = 16;
 let i = 0;
 
+
 function makeTheBox (gridNumber) {
+    i = 0;
     while ( i < (gridNumber ** 2)) {
         var square = document.createElement("div");
         square.classList.add("gridSquare");
@@ -11,19 +14,38 @@ function makeTheBox (gridNumber) {
         container.appendChild(square);
         i++;
     }
+  var gridItems = document.querySelectorAll('.gridSquare');
+  
+  gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', function (e) {
+      e.target.style.backgroundColor = 'black';
+  }));
 }
 
 makeTheBox(gridNumber);
 
-var gridItems = document.querySelectorAll('.gridSquare');
-gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', function (e) {
-      e.target.style.backgroundColor = 'black';
-  }));
+
+
+function clearGrid () {
+    var gridNumber = prompt('How large would you like the grid (no larger than 100)', '');
+    while (isNaN(gridNumber) || (gridNumber > 100)){
+        gridNumber = prompt('How large would you like the grid (no larger than 100)');
+    }
+    let allCells = document.querySelectorAll(".gridSquare").forEach(gridItem => {
+        gridItem.style.backgroundColor = "white";
+    })
+    while (container.firstChild)  {
+        container.removeChild(container.lastChild);
+    }
+    makeTheBox(gridNumber);
+}
+
+
+
+clearButton.addEventListener('click', clearGrid);
+
 
 /*
-hover
-clear button 
-prompt
+
 size slider
 
 
